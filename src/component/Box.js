3042186df00a833e3c2d3032e0b37fb3bb410813
changwoo Jeong <jcw9828@gmail.com>
2025-04-02 20:33:39 +0900

    [add] init img

--- src/component/Box.js
+++ src/component/Box.js
@@ -17,12 +17,17 @@ const Box = (props) => {
   return (
     <div className={`box ${result}`}>
       <h1>{props.title}</h1>
-      {props.item && (
-        <div className="imgContainer">
-          <img className="item-img" src={props.item.img} />
-        </div>
-      )}
-      <h1>{result}</h1>
+      <div className="imgContainer">
+        <img
+          className="item-img"
+          src={
+            props.item
+              ? props.item.img
+              : "https://cdn.pixabay.com/photo/2017/02/13/01/26/the-question-mark-2061539_1280.png"
+          }
+        />
+      </div>
+      <h1 style={{ height: 50, textAlign: "center" }}>{result}</h1>
     </div>
   );
 };
